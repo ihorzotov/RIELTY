1e92130075001225f7efb1b7152dec474883b79b
Ihor <vippwp7@gmail.com>
2018-12-07 23:41:01 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,46 +25,60 @@ $(document).ready(function () {
     }, 3000);
   });
 
-function NumCounter() {
-  $('.main-sixth__card').each(function(key,item){
-    var numbStart = parseInt($(item).attr('data-num'));
-    console.log(numbStart)
-    $({numberValue: 0}).animate({numberValue: numbStart}, {
-      duration: 1000,
-      easing: "linear",
-      
-      step: function(val) {
-        // $(item).find('.num').html(Math.ceil(val));
-        $(item).find('.num').html(val.toFixed(0));
-      }
+function MediaJs(){
+  if ($(window).width() > 1170) {
+    function NumCounter() {
+      $('.main-sixth__card').each(function(key,item){
+        var numbStart = parseInt($(item).attr('data-num'));
+        $({numberValue: 0}).animate({numberValue: numbStart}, {
+          duration: 1000,
+          easing: "linear",
+          
+          step: function(val) {
+            // $(item).find('.num').html(Math.ceil(val));
+            $(item).find('.num').html(val.toFixed(0));
+          }
+        });
+      });
+    };
+
+    jQuery(function($) {
+      var doAnimations = function() {
+        var offset = $(window).scrollTop() + $(window).height(),
+            $animatables = $('.animation'),
+            jsCounter = $('.main-sixth__card.js-counter');
+
+        jsCounter.each(function(i) {
+          var $animatable = $(this);
+          if (($animatable.offset().top + $animatable.height() - 300) < offset) {
+            jsCounter.removeClass('js-counter');
+            NumCounter();
+          }
+        });
+        $animatables.each(function(i) {
+          var $animatable = $(this);
+          console.log($animatable.offset().top + $animatable.height(),offset);
+          if (($animatable.offset().top + $animatable.height() - 300) < offset) {
+            $animatable.removeClass('animation');
+          }
+        });
+        
+      };
+
+      $(window).on('scroll', doAnimations);
+      $(window).trigger('scroll');
+
     });
-  });
-};
-
-jQuery(function($) {
-  var doAnimations = function() {
-    var offset = $(window).scrollTop() + $(window).height(),
-        $animatables = $('.animation'),
-        jsCounter = $('.main-sixth__card.js-counter');
-
-    jsCounter.each(function(i) {
-      var $animatable = $(this);
-      if (($animatable.offset().top + $animatable.height() - 300) < offset) {
-        jsCounter.removeClass('js-counter');
-        NumCounter();
-      }
-    });
-    $animatables.each(function(i) {
-      var $animatable = $(this);
-      if (($animatable.offset().top + $animatable.height() - 300) < offset) {
-        $animatable.removeClass('animation');
-      }
-    });
-    
-  };
-
-  $(window).on('scroll', doAnimations);
-  $(window).trigger('scroll');
-
+  }else{
+    $('.js-counter,.animation').removeClass('js-counter, animation');
+    $('.main-sixth__card').each(function(key,item){
+      var numb = $(item).attr('data-num');
+      $(item).find('.num').html(numb);
+    })
+  }
+}
+MediaJs();
+$(window).resize(function(){
+  MediaJs();
 });
 });
